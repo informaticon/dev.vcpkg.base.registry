d1380f604ff4d3a299a090108780addd21068045
Micha Wehrli <micha.wehrli@informaticon.com>
2024-07-03 13:35:19 +0200

    bump pbni-framework

--- ports/pbni-framework/portfile.cmake
+++ ports/pbni-framework/portfile.cmake
@@ -27,7 +27,7 @@ vcpkg_cmake_configure(
     -DINSTALL_CMAKEDIR="lib/share/pbni-framework"
 )
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup("")
+vcpkg_cmake_config_fixup()
 vcpkg_copy_pdbs()
 
 # file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
