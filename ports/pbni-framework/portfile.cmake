ed8fa8aae627c355f6f1658c073ec915196dffd6
Micha Wehrli <micha.wehrli@informaticon.com>
2024-07-03 12:24:54 +0200

    bump pbni-framework

--- ports/pbni-framework/portfile.cmake
+++ ports/pbni-framework/portfile.cmake
@@ -25,7 +25,7 @@ vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"
 )
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/fizz)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/pbni-framework)
 vcpkg_copy_pdbs()
 
 # file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
